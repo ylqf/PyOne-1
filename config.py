49e4ce8d23ceed7dcbd06efdd4d8d3a2807ea333
Abbey <tumbzzc@qq.com>
2018-08-16 10:58:51 +0800

    Update config.py

--- config.py
+++ config.py
@@ -20,3 +20,6 @@ od_type='business' #国际版：bussiness; 世纪互联版：bussiness_21v
 #下载链接过期时间
 downloadUrl_timeout=30*60 #默认30分钟过期
 
+#onedrive个人页的域名。国际版为com结尾，世纪互联版为cn结尾，最后面一定要带/
+#不知道自己的onedrive个人域名的，自己登陆onedrive查看
+app_url='https://yourname-my.sharepoint.com/' 
