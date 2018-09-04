5f25278389bf543965232a1c959feb31ef86f027
Abbey <tumbzzc@qq.com>
2018-09-04 11:58:17 +0800

    更新上传逻辑，上传文件之后即刻添加到数据库中，无需重新更新文件

--- function.py
+++ function.py
@@ -306,8 +306,7 @@ def _upload(filepath,remote_path): #remote_path like 'share/share.mp4'
         print(data.get('error').get('message'))
         return False
     elif data.get('@content.downloadUrl'):
-        print('upload success!')
-        return data.get('@content.downloadUrl')
+        return data
     else:
         print(data)
         return False
@@ -331,7 +330,7 @@ def _upload_part(uploadUrl, filepath, offset, length):
         data=json.loads(r.content)
         if data.get('@content.downloadUrl'):
             print(u'{} upload success!'.format(filepath))
-            return {'status':'success','msg':'all upload success','code':0}
+            return {'status':'success','msg':'all upload success','code':0,'info':data}
         elif r.status_code==202:
             offset=data.get('nextExpectedRanges')[0].split('-')[0]
             return {'status':'success','msg':'partition upload success','code':1,'offset':offset}
@@ -370,7 +369,42 @@ def _GetAllFile(parent_id="",parent_path="",filelist=[]):
     return filelist
 
 
-
+def AddResource(remote_path,data):
+    remote_dir,filename='/'.join(remote_path.split('/')[:-1]),remote_path.split('/')[-1]
+    if remote_dir.startswith('/'):
+        remote_dir=remote_dir[1:]
+    if remote_dir=='':
+        item={}
+        item['type']='file'
+        item['name']=convert2unicode(filename)
+        item['id']=convert2unicode(data.get('id'))
+        item['size']=humanize.naturalsize(data.get('size'), gnu=True)
+        item['lastModtime']=humanize.naturaldate(parse(data.get('lastModifiedDateTime')))
+        item['grandid']=0
+        item['parent']=''
+        items.insert_one(item)
+    if items.find_one({'grandid':0,'type':'folder','name':remote_dir.split('/')[0]}):
+        parent_id=0
+        parent_path=''
+        for idx,p in enumerate(remote_dir.split('/')):
+            if parent_id==0:
+                parent=items.find_one({'name':p,'grandid':idx})
+                parent_id=parent['id']
+                parent_path='/'.join([parent_path,parent['name']])
+            else:
+                parent=items.find_one({'name':p,'grandid':idx,'parent':parent_id})
+                parent_id=parent['id']
+                parent_path='/'.join([parent_path,parent['name']])
+        grandid=idx+1
+        item={}
+        item['type']='file'
+        item['name']=convert2unicode(filename)
+        item['id']=convert2unicode(data.get('id'))
+        item['size']=humanize.naturalsize(data.get('size'), gnu=True)
+        item['lastModtime']=humanize.naturaldate(parse(data.get('lastModifiedDateTime')))
+        item['grandid']=grandid
+        item['parent']=parent_id
+        items.insert_one(item)
 
 def CreateUploadSession(path):
     token=GetToken()
@@ -403,7 +437,7 @@ def UploadSession(uploadUrl, filepath):
         #上传完成
         if code==0:
             break
-            return True
+            return result['info']
         #分片上传成功
         elif code==1:
             offset=result['offset']
@@ -426,23 +460,22 @@ def Upload(filepath,remote_path=None):
         remote_path=os.path.join(remote_path,os.path.basename(filepath))
     if remote_path.startswith('/'):
         remote_path=remote_path[1:]
-    print('local file path:  {}\nremote file path:  {}'.format(filepath,remote_path))
     if _filesize(filepath)<10485760:
-        print('upload small file')
         result=_upload(filepath,remote_path)
-        if result!=False:
-            print('upload success')
+        if result==False:
+            print(u'{} upload fail!'.format(filepath))
         else:
-            print('upload fail')
+            print(u'{} upload success!'.format(filepath))
+            AddResource(remote_path,result)
     else:
-        print('upload large file')
         session_data=CreateUploadSession(remote_path)
         if session_data==False:
             print('file exists')
         else:
             if session_data.get('uploadUrl'):
                 uploadUrl=session_data.get('uploadUrl')
-                UploadSession(uploadUrl,filepath)
+                data=UploadSession(uploadUrl,filepath)
+                AddResource(remote_path,data)
             else:
                 print(session_data)
                 print('create upload session fail! {}'.format(remote_path))
